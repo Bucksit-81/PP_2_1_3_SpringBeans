36f86deb6b15e455e70b6f84ec913f7628ec0aa7
Bucksit-81 <5229290@gmail.com>
2023-04-19 01:38:37 +0300

    commit

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -3,13 +3,16 @@ package app.model;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Cat extends Animal {
+public class Dog extends Animal {
     @Override
     void say() {
 
     }
+    public Dog() {
+    }
+
     @Override
     public String toString() {
-        return "I'm a Cat";
+        return "I'm a dog";
     }
 }
